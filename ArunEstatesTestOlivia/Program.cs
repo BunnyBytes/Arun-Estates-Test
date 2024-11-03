610386f9ac47cac2052b17667cf093c36bef4a0a
BunnyBytes <o.crichton96@gmail.com>
2024-11-03 01:18:35 +0000

    Removed/updated boilerplate VS code

--- ArunEstatesTestOlivia/Program.cs
+++ ArunEstatesTestOlivia/Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
